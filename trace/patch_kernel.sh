329485c2082bb046c55df3d8673468172fc14d3f
hellozhuo <zhuo.su@amd.com>
2026-03-24 19:06:12 +0000

    Add -v for patch

--- trace/patch_kernel.sh
+++ trace/patch_kernel.sh
@@ -1,5 +1,5 @@
 INSTALL_PATH=$1
-cp vllm/model_executor/__init__.py $INSTALL_PATH/vllm/model_executor/__init__.py
-cp vllm/model_executor/layers/fla/ops/fused_rearrange_recurrent.py $INSTALL_PATH/vllm/model_executor/layers/fla/ops/fused_rearrange_recurrent.py
-cp vllm/model_executor/layers/fla/ops/fused_conv1d_rearrange_recurrent.py $INSTALL_PATH/vllm/model_executor/layers/fla/ops/fused_conv1d_rearrange_recurrent.py
-cp vllm/model_executor/models/qwen3_next.py $INSTALL_PATH/vllm/model_executor/models/qwen3_next.py
+cp -v vllm/model_executor/__init__.py $INSTALL_PATH/vllm/model_executor/__init__.py
+cp -v vllm/model_executor/layers/fla/ops/fused_rearrange_recurrent.py $INSTALL_PATH/vllm/model_executor/layers/fla/ops/fused_rearrange_recurrent.py
+cp -v vllm/model_executor/layers/fla/ops/fused_conv1d_rearrange_recurrent.py $INSTALL_PATH/vllm/model_executor/layers/fla/ops/fused_conv1d_rearrange_recurrent.py
+cp -v vllm/model_executor/models/qwen3_next.py $INSTALL_PATH/vllm/model_executor/models/qwen3_next.py
