b781b4b0573dc8a5184f6fe73da4d14316b65cb5
hellozhuo <zhuo.su@amd.com>
2026-03-25 10:55:35 +0000

    Update patch_kernel.sh

--- trace/patch_kernel.sh
+++ trace/patch_kernel.sh
@@ -2,4 +2,5 @@ INSTALL_PATH=$1
 cp -v vllm/model_executor/__init__.py $INSTALL_PATH/vllm/model_executor/__init__.py
 cp -v vllm/model_executor/layers/fla/ops/fused_rearrange_recurrent.py $INSTALL_PATH/vllm/model_executor/layers/fla/ops/fused_rearrange_recurrent.py
 cp -v vllm/model_executor/layers/fla/ops/fused_conv1d_rearrange_recurrent.py $INSTALL_PATH/vllm/model_executor/layers/fla/ops/fused_conv1d_rearrange_recurrent.py
+cp -v vllm/model_executor/layers/fla/ops/fused_sigmoid_gating.py $INSTALL_PATH/vllm/model_executor/layers/fla/ops/fused_sigmoid_gating.py
 cp -v vllm/model_executor/models/qwen3_next.py $INSTALL_PATH/vllm/model_executor/models/qwen3_next.py
