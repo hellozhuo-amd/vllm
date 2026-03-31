876702a17d720e268908c1f3441daf15a2ed5cae
hellozhuo <zhuo.su@amd.com>
2026-03-31 17:30:45 +0000

    Revert to zhuo/fusion_4_tres_patch_fixed

--- vllm/model_executor/models/qwen3_next.py
+++ vllm/model_executor/models/qwen3_next.py
@@ -605,12 +605,10 @@ class Qwen3NextGatedDeltaNet(nn.Module, MambaBase):
         # Reshape input data into 2D tensor
         core_attn_out = core_attn_out.reshape(-1, core_attn_out.shape[-1])
         z = z.reshape(-1, z.shape[-1])
-        #core_attn_out = self.norm(core_attn_out, z)
+        core_attn_out = self.norm(core_attn_out, z)
         core_attn_out = core_attn_out.reshape(z_shape_og)
         core_attn_out = rearrange(core_attn_out, "... h d -> ... (h d)")
-        # output[:num_tokens], _ = self.out_proj(core_attn_out)
-        w = torch.rand(self.value_dim//self.tp_size, self.hidden_size, dtype=output.dtype, device=output.device)
-        output[:num_tokens], _ = torch.matmul(core_attn_out, w)
+        output[:num_tokens], _ = self.out_proj(core_attn_out)
 
     def _forward_core(
         self,
