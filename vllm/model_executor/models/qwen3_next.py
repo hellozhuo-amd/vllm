ce161912937cba0356a264dea0728ce1a2a233e9
hellozhuo <zhuo.su@amd.com>
2026-03-31 11:19:41 +0000

    Separate core_attn_out and z

--- vllm/model_executor/models/qwen3_next.py
+++ vllm/model_executor/models/qwen3_next.py
@@ -572,23 +572,23 @@ class Qwen3NextGatedDeltaNet(nn.Module, MambaBase):
         # Note: we should not use torch.empty here like other attention backends,
         # see discussions in https://github.com/vllm-project/vllm/pull/28182
 
-        #core_attn_out = torch.zeros(
-        #    (num_tokens, self.num_v_heads // self.tp_size, self.head_v_dim),
-        #    dtype=hidden_states.dtype,
-        #    device=hidden_states.device,
-        #)
-        #z = torch.zeros(
-        #    (num_tokens, self.num_v_heads // self.tp_size, self.head_v_dim),
-        #    dtype=projected_states_qkvz.dtype,
-        #    device=projected_states_qkvz.device,
-        #)
+        core_attn_out = torch.zeros(
+            (num_tokens, self.num_v_heads // self.tp_size, self.head_v_dim),
+            dtype=hidden_states.dtype,
+            device=hidden_states.device,
+        )
+        z = torch.zeros(
+            (num_tokens, self.num_v_heads // self.tp_size, self.head_v_dim),
+            dtype=projected_states_qkvz.dtype,
+            device=projected_states_qkvz.device,
+        )
 
         ## create buffer for core_attn_out and z, both with shape (num_tokens, self.num_v_heads // self.tp_size, self.head_v_dim)
-        core_attn_out, z = self.prepare_output_buffer(
-                num_tokens, 
-                dtype=projected_states_qkvz.dtype, 
-                device=projected_states_qkvz.device,
-        )
+        #core_attn_out, z = self.prepare_output_buffer(
+        #        num_tokens, 
+        #        dtype=projected_states_qkvz.dtype, 
+        #        device=projected_states_qkvz.device,
+        #)
 
         torch.ops.vllm.gdn_attention_core(
             projected_states_qkvz,
